0eec5219efde4316f69ef6f255c70643320993a8
Александр <a.a.popov2001@gmail.com>
2023-11-09 10:09:33 +0300

    delete history BotModel.cs GptService.cs

---
+++ Main/BotModel.cs
@@ -0,0 +1,18 @@
+﻿using IoC;
+using Models;
+using Telegram.Bot;
+
+namespace Main;
+
+public class BotModel
+{
+	private static TelegramBotClient? Client { get; set; }
+
+	public static TelegramBotClient GetTelegramBot()
+	{
+		if (Client is not null) { return Client; }
+		
+		Client = new TelegramBotClient(IoCContainer.GetConstant<string>(Constants.BOT_KEY));
+		return Client;
+	}
+}
---
+++ Service/GptService.cs
@@ -0,0 +1,51 @@
+﻿using IoC;
+using Models;
+using Models.KindOfChats;
+using OpenAI;
+using OpenAI.Managers;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels.ResponseModels;
+
+namespace Service;
+
+public class GptService : IGptService
+{
+	private readonly string _gptSecretKey;
+
+	public GptService()
+	{
+		_gptSecretKey = IoCContainer.GetConstant<string>(Constants.GPT_KEY);
+	}
+	public async Task<string> AskQuestionAsync(List<History>? history, string question)
+	{
+		OpenAIService openAiService = new(new OpenAiOptions() {
+				ApiKey = _gptSecretKey
+		});
+		ChatCompletionCreateResponse completionResult = 
+				await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest {
+						Messages = SetChatMessage(history, question),
+						Model = OpenAI.ObjectModels.Models.ChatGpt3_5Turbo
+				});
+		if (completionResult.Successful) {
+			return completionResult.Choices.First().Message.Content;
+		}
+		if (completionResult.Error == null) {
+			throw new Exception("Нейросетка в ошибке прислала NULL!");
+		}
+		return completionResult.Error.Message ?? "Упс, ошибочка...";
+	}
+
+	public List<ChatMessage> SetChatMessage(List<History>? history, string question)
+	{
+		List<ChatMessage> list = new();
+		foreach (History t in history) {
+			if (t is not null) {
+				list.Add(ChatMessage.FromUser(t.UserMessage));
+				list.Add(ChatMessage.FromAssistant(t.BotMessage));
+			}
+			list.Add(ChatMessage.FromUser(question));
+			return list;
+		}
+		return list;
+	}
+}
